1f121f8ac65a39bafe1ce807ae13ace99d54567f
Dylan Cairns <dcairns@indicalab.com>
2021-11-29 14:46:07 -0700

    further simplified

--- src/index.js
+++ src/index.js
@@ -1,11 +1,73 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
+import { withFormik } from "formik";
+import { object, string, number } from "yup";
 
-import App from "./App";
+const MyForm = (props) => {
+  const { values, handleChange, handleBlur, handleSubmit } = props;
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <input
+          type="text"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values["values"]["1"]}
+          name="values.1"
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values["values"]["5"]}
+          name="values.5"
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values["values"]["name"]}
+          name="values.name"
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+const MyEnhancedForm = withFormik({
+  mapPropsToValues: () => ({
+    imageID: 82,
+    values: {
+      1: "",
+      5: "",
+      name: "",
+    },
+  }),
+
+  validationSchema: object().shape({
+    imageID: number(),
+    values: object().shape({
+      1: string().required("Required"),
+      5: string().required("Required"),
+      name: string().required("Required"),
+    }),
+  }),
+
+  handleSubmit: () => {},
+
+  displayName: "BasicForm",
+})(MyForm);
+
+const App = () => (
+  <div className="app">
+    <MyEnhancedForm />
+  </div>
 );
+
+render(<App />, document.getElementById("root"));
